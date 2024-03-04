98635f5b594528b4ed9f745c63877502bf1023df
vignesh <vigneshfornavy@gmail.com>
2024-03-04 12:17:06 +0530

    Implemented Wacth Session

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { closeMenu } from '../utils/appSlice';
 
 const Watch = () => {
+
+  const [searchParams] = useSearchParams();
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(closeMenu());
+  },[])
+
   return (
-    <div className='col-span-10'>Watch</div>
+    <div className='col-span-10 pt-2 px-5'>
+      <div className='p-2 m-5'>
+      <iframe className='rounded-md shadow-lg' width="940" height="540" src={"https://www.youtube.com/embed/"+searchParams.get("v")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      </div>
+      
+
+    </div>
   )
 }
 
